857aac65f660d2353e20dd8e9393f2b31be0e17d
Jim Owen Bognalbal <147787685+KBJOwen29@users.noreply.github.com>
2026-08-27 17:41:16 +0800

    Update BankAccounts.py

--- Bank Account Management/BankAccounts.py
+++ Bank Account Management/BankAccounts.py
@@ -1,17 +1,23 @@
+# For Accounts
 class Account:
     def __init__(self, account_number, account_holder, balance=0.0):
         self.account_number = account_number
         self.account_holder = account_holder
         self.balance = balance
+
+    # Gets the balance of the signed in account
     def get_balance(self):
         return self.balance
-    
+
+    # Sets the name of the Account
     def set_Name(self, new_name):
         self.account_holder = new_name
-    
+
+    # Sets the Account Number
     def set_accountNumber(self, new_account_number):
         self.account_number = new_account_number
-    
+
+    # Gets the information of the Account
     def get_AccountInfo(self):
         return {
             "Account Number": self.account_number,
@@ -19,6 +25,7 @@ class Account:
             "Balance": self.balance
         }
         
+    # Sets an initial balance to a newly created account    
     def set_initial_balance(self, amount):
         if amount >= 0:
             self.balance = amount
